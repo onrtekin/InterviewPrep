1a636fef9cf8e859026f19701052ad8566c30908
Oner Tekin <tekinoner37@gmail.com>
2022-04-26 16:28:13 +0300

    26.04.2022

--- src/interviewQuestions8/Q05_WrapperClassTask.java
+++ src/interviewQuestions8/Q05_WrapperClassTask.java
@@ -9,5 +9,23 @@ public class Q05_WrapperClassTask {
     // int kabul eden bir metot yarat
     // ve bu int'nin iki katını döndürür
     // bu yöntemi wrapper sınıfıyla overload et : Integer
+    public static void main(String[] args) {
+
+        twice(5);
+
+        System.out.println(twice(5));
+        Integer sayi=10;
+        System.out.println(twice(sayi));
+    }
+
+    private static int twice(int sayi) {
+
+    return sayi*2;
+    }
+    private static Integer twice(Integer sayi) {
+
+        return sayi*2;
+    }
+
 
 }
--- src/interviewQuestions8/Q06_AddedOrNotTask.java
+++ src/interviewQuestions8/Q06_AddedOrNotTask.java
@@ -1,5 +1,10 @@
 package interviewQuestions8;
 
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
 public class Q06_AddedOrNotTask {
 
     // create a list of numbers 1, 3, 1, 2, 4, 5, 3 --> 2 duplicates
@@ -9,4 +14,28 @@ public class Q06_AddedOrNotTask {
     // 1, 3, 1, 2, 4, 5, 3 sayılarının bir listesini oluşturun --> 2 kopya
     // kaç tane kopyamız olduğunu sayin
     // yinelenen değerlerin ne olduğunu öğrenin
+    public static void main(String[] args) {
+        List<Integer> sayilar=new ArrayList<>();
+        sayilar.add(1);
+        sayilar.add(3);
+        sayilar.add(1);
+        sayilar.add(2);
+        sayilar.add(4);
+        sayilar.add(5);
+        sayilar.add(3);
+        int tekrarliElemanSayisi=0;
+        Set<Integer> tekOlanlar=new HashSet<>();
+        List<Integer>tekrarliElemanlar=new ArrayList<>();
+        for (int i = 0; i < sayilar.size(); i++) {
+            boolean ekliMi=tekOlanlar.add(sayilar.get(i));//ekli olma durumu uniqe elemanlarda var olmasidir
+            if(!ekliMi){//unique elemanlara eklenmediyse o eleman duplicate tir.
+                tekrarliElemanSayisi++;
+                tekrarliElemanlar.add(sayilar.get(i));
+            }
+
+        }
+        System.out.println("Tekrarli eleman Sayisi  : "+tekrarliElemanSayisi);
+        System.out.println("Tekrarlanan Elemanlar : "+tekrarliElemanlar);
+
+    }
 }
